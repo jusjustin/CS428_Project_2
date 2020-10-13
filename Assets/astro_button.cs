9508ee6e3532d7830b5861b61e48edacfd7566ae
jusjustin <justin4@uic.edu>
2020-10-12 20:30:04 -0500

    Alpha
    Alpha

---
+++ Assets/astro_button.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+ 
+public class astro_button : MonoBehaviour
+{
+ 
+    public GameObject vbBtnObj;
+    public AudioSource audioSource;
+ 
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("AstroButton");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+    }
+ 
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        audioSource.Play();
+        Debug.Log("Button pressed");
+    }
+ 
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button released");
+    }
+}
